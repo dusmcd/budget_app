fc7b8877ec915e1de1e39151be22c0e4184b4a3b
dustin <dustinmcdowell1989@gmail.com>
2016-06-02 20:55:50 -0600

    add categories with button

--- budget.js
+++ budget.js
@@ -1,28 +1,55 @@
 function init () {
 	addCategory();
-	sumExp();
-};
+	sumItems(".expense-input", "#total-exp");
+	sumItems(".income-input", "#total-inc");
+	netCash();
+};	
 
 function addCategory() {
 	$('button').on("click", function() {
-		$("#add-to").append("<h1>It worked!</h1>");
-	})
+		var category = prompt("Please Specify Category");
+		$(".add-to").append('<div class="row spacing">\
+		<div class="col-lg-6">\
+			<label>'+ category + '</label>\
+		</div>\
+		<div class="col-lg-6">\
+			<input type="number" class="expense-input">\
+		</div>\
+	</div>');
+
+	sumItems(".expense-input", "#total-exp");
+	netCash();
+
+	});
 };
 
-function sumExp() {
-	var input = document.querySelectorAll("input");
+
+
+function sumItems(inputNum, outputNum) {
+	var input = document.querySelectorAll(inputNum);
 	var sum = 0;
-	$("input").keyup(function() {
+	$(inputNum).on("change", function() {
 		sum = 0;
 		for(var i = 0; i < input.length; i++ ) {
 			sum += Number(input[i].value);
 		}
 
-		$("#total-exp").text(sum);		
+		$(outputNum).text(sum);		
 	})
 
+};
 
-}
+function netCash() {
+	$('input').on("change", function() {
+		var expenses = Number(document.querySelector("#total-exp").textContent);
+		var income = Number(document.querySelector("#total-inc").textContent);
+		var netCashTotal = income - expenses;
+		$("#net-cash").text(netCashTotal);		
+	});
+
+};
+
+
 
 
 $(document).ready(init);
